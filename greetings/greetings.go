ffa3807f0651440a23bbd58f26206a7b0b923a2b
smburdick <samuelmburdick@gmail.com>
2021-06-01 11:14:50 -0700

    Error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+  "fmt"
+  "errors"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+func Hello(name string) (string, error) {
+  if name == "" {
+    return "", errors.New("empty name")
+  }
+  // Return a greeting that embeds the name in a message.
+  message := fmt.Sprintf("Hi, %v. Welcome!", name)
+  return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-    "fmt"
-
-    "example.com/greetings"
+  "fmt"
+  "log"
+  "time"
+  "example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Asswipe")
-    fmt.Println(message)
+  log.SetPrefix("greetings: ")
+  log.SetFlags(0)
+  // Get a greeting message and print it.
+  message, err := greetings.Hello("")
+  if err != nil {
+    log.Fatal("[" + time.Now().Format("2006-01-02 15:04:05") + "] ", err)
+  }
+  fmt.Println(message)
 }
